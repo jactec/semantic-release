954495237a303a25819e218112092d35c27fd16d
jactec <someone@somewhere.nowhere>
2022-01-18 14:33:46 +1100

    fix: patch on develop commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Version 1.1.1
+          Learn React - Version 1.1.2
         </a>
       </header>
     </div>
